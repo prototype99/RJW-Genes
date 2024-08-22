1489edc406d64f7295ec3259503e281bf3691075
Seirra Blake <sophietheopossum@yandex.ru>
2024-08-22 18:40:35 +0100

    add missing defs

--- Source/Genes/Genitalia/Defs/GenitaliaTypeExtension.cs
+++ Source/Genes/Genitalia/Defs/GenitaliaTypeExtension.cs
@@ -6,13 +6,27 @@ namespace RJW_Genes
 {
     public class GenitaliaTypeExtension : DefModExtension
     {
-        public HediffDef_PartBase penis;
+        public HediffDef_SexPart penis;
+        
+        public HediffDef_SexPart GolemPenis;
+        
+        public HediffDef_SexPart MarinePenis;
+        
+        public HediffDef_SexPart NecroPenis;
+        
+        public HediffDef_SexPart OrcPenis;
+        
+        public HediffDef_SexPart PigPenis;
+        
+        public HediffDef_SexPart TentaclePenis;
+        
+        public HediffDef_SexPart vagina;
+        
+        public HediffDef_SexPart GhostVagina;
 
-        public HediffDef_PartBase vagina;
+        public HediffDef_SexPart anus;
 
-        public HediffDef_PartBase anus;
-
-        public HediffDef_PartBase breasts;
+        public HediffDef_SexPart breasts;
 
     }
 }
